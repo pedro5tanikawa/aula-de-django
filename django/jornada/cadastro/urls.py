c27af46de6f82258290d7bbdb4f6abbc2d1159d8
pedro5tanikawa <phlkawa@gmail.com>
2025-12-15 20:49:30 -0300

    mudanças da aula de 15/12

--- django/jornada/cadastro/urls.py
+++ django/jornada/cadastro/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.cadastro, name='cadastro'),
+    path('gravar/', views.gravar, name="gravar"),
+    path('mostrar/', views.exibe, name="mostrar"),
+    
 ]
--- django/jornada/cadastro/views.py
+++ django/jornada/cadastro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from cadastro.models import Pessoa
+
 # Create your views here.
 def cadastro(request):
     contexto = {
@@ -9,4 +11,23 @@ def cadastro(request):
         request,
         'cadastro/index.html',
         contexto,
+    )
+
+def gravar(request): #funçao para salvar os dados para a tabela
+    nova_pessoa = Pessoa()
+    nova_pessoa.nome = request.POST.get('nome')
+    nova_pessoa.idade = request.POST.get('idade')
+    nova_pessoa.email = request.POST.get('email')
+    nova_pessoa.save()
+    
+    return cadastro(request)
+
+def exibe(request):
+    exibe_pessoas = {
+        "pessoas": Pessoa.objects.all()
+    }
+    return render(
+        request,
+        'cadastro/mostrar.html',
+        exibe_pessoas,
     )
